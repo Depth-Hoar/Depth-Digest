f11e976d3237ae3bd1ff9e7a230e446312ac2352
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-04-13 14:10:16 -0600

    logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#66339`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/depth-logo-White-crop.png`, // This path is relative to the root of the site.
       },
     },
   ],
